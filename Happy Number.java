42d2916cb37e1b3b80018d5f3ba968f08c6f7982
Nikita Verma <156908148+Nikitav0716@users.noreply.github.com>
2025-02-02 22:52:01 +0530

    Create Happy Number.java

---
+++ Happy Number.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isHappy(int n) {
+        HashSet<Integer> set = new HashSet<>();
+        while(n!=1 && !set.contains(n)){
+            set.add(n);
+            n=getnext(n);
+        }
+        return n==1;
+    }
+
+    private int getnext(int n){
+        int sum = 0;
+        while(n>0){
+            int digit=n%10;
+            sum += digit*digit;
+            n=n/10;
+        }
+        return sum;
+    }
+}
